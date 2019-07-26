28a7d864dd7370b22102eed32816bc23a0ab6847
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-07-26 15:50:45 -0400

    a bunch of deployment attempts and travis linking attempsts from the bookdown page chapter 6.2

---
+++ _deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "shirokuriwaki@gmail.com"
+git config --global user.name "Shiro Kuriwaki"
+
+# clone the repository to the book-deploy directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-deploy
+cd book-deploy
+git rm -rf *
+cp -r ../_book/* ./
+git add --all *
+git commit -m "Update the book"
+git push -q origin gh-pages
