d74ea41aa0ce4d98b0056469907aec7c59672a76
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-07-29 17:15:05 -0400

    un-indent

--- _deploy.sh
+++ _deploy.sh
@@ -13,9 +13,7 @@ git config --global user.email "shirokuriwaki@gmail.com"
 git config --global user.name "Shiro Kuriwaki"
 
 # clone the repository to the book-deploy directory
-git clone -b gh-pages \
-  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
-  book-deploy
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-deploy
 cd book-deploy
 git rm -rf *
 cp -r ../_book/* ./
