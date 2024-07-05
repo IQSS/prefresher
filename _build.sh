6206a4653cb5cfb462c86cc24fb73bdb12e4d1bd
Christopher Kenny <ctk44@cornell.edu>
2024-07-05 01:21:35 -0400

    remove files that aren't used for quarto or GHA

--- _build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Called by .travis.yml. Compiles bookdown in HTML form. All R packages needed to 
-# be listed in the separate DESCRIPTION file to be installed.
-
-set -ev
-
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
